9eab950ccd810d77f12ab3561688834427d7c103
Southboy <menkachi85@gmail.com>
2024-03-31 00:46:21 +0100

    Changed class to className

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function NewsItem(props) {
   return (
     <div>
-      <div className="card" style={{ width: "18rem" }}>
+      <div className="card">
         <img src={props.img} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
           <p className="card-text">{props.description}</p>
-          <a href={props.url} target="_blank" className="btn btn-primary">
+          <a href={props.url} className="btn btn-primary">
             Go somewhere
           </a>
         </div>
